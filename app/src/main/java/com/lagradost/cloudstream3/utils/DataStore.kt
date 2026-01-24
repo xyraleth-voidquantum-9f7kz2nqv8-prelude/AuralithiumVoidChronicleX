90bf4f1eb7e8d5d9e39d44095bceb581de3e146e
xyraleth-voidquantum-9f7kz2nqv8-prelude <xyralethvoidquantum@gmail.com>
2026-01-25 04:59:58 +0700

    Update DataStore.kt

--- app/src/main/java/com/lagradost/cloudstream3/utils/DataStore.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/DataStore.kt
@@ -3,48 +3,62 @@ package com.lagradost.cloudstream3.utils
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
-import androidx.core.content.edit
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.kotlin.kotlinModule
-import com.lagradost.cloudstream3.CloudStreamApp
+import com.lagradost.cloudstream3.CloudStreamApp.Companion.getKeyClass
+import com.lagradost.cloudstream3.CloudStreamApp.Companion.removeKey
+import com.lagradost.cloudstream3.CloudStreamApp.Companion.setKeyClass
 import com.lagradost.cloudstream3.mvvm.logError
-import kotlin.reflect.KProperty
 import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
+import androidx.core.content.edit
 
 const val DOWNLOAD_HEADER_CACHE = "download_header_cache"
+
+//const val WATCH_HEADER_CACHE = "watch_header_cache"
 const val DOWNLOAD_EPISODE_CACHE = "download_episode_cache"
 const val VIDEO_PLAYER_BRIGHTNESS = "video_player_alpha_key"
 const val USER_SELECTED_HOMEPAGE_API = "home_api_used"
 const val USER_PROVIDER_API = "user_custom_sites"
 const val PREFERENCES_NAME = "rebuild_preference"
 
-/** =================
- * Preference Delegate
- * ================= */
+// TODO degelgate by value for get & set
+
 class PreferenceDelegate<T : Any>(
-    val key: String, val default: T
+    val key: String, val default: T //, private val klass: KClass<T>
 ) {
     private val klass: KClass<out T> = default::class
+
+    // simple cache to make it not get the key every time it is accessed, however this requires
+    // that ONLY this changes the key
     private var cache: T? = null
 
     operator fun getValue(self: Any?, property: KProperty<*>) =
-        cache ?: CloudStreamApp.context?.getKey(key, klass.java)?.also { cache = it } ?: default
+        cache ?: getKeyClass(key, klass.java).also { newCache -> cache = newCache } ?: default
 
-    operator fun setValue(self: Any?, property: KProperty<*>, t: T?) {
+    operator fun setValue(
+        self: Any?,
+        property: KProperty<*>,
+        t: T?
+    ) {
         cache = t
-        if (t == null) CloudStreamApp.context?.removeKey(key)
-        else CloudStreamApp.context?.setKey(key, t)
+        if (t == null) {
+            removeKey(key)
+        } else {
+            setKeyClass(key, t)
+        }
     }
 }
 
-/** =================
- * Editor Helper
- * ================= */
-data class Editor(val editor: SharedPreferences.Editor) {
+/** When inserting many keys use this function, this is because apply for every key is very expensive on memory */
+data class Editor(
+    val editor: SharedPreferences.Editor
+) {
+    /** Always remember to call apply after */
     fun <T> setKeyRaw(path: String, value: T) {
         @Suppress("UNCHECKED_CAST")
-        if (value is Set<*> && value.all { it is String }) {
+        if (isStringSet(value)) {
             editor.putStringSet(path, value as Set<String>)
         } else {
             when (value) {
@@ -57,51 +71,85 @@ data class Editor(val editor: SharedPreferences.Editor) {
         }
     }
 
+    private fun isStringSet(value: Any?): Boolean {
+        if (value is Set<*>) {
+            return value.filterIsInstance<String>().size == value.size
+        }
+        return false
+    }
+
     fun apply() {
         editor.apply()
         System.gc()
     }
 }
 
-/** =================
- * DataStore Object
- * ================= */
 object DataStore {
-    val mapper: JsonMapper = JsonMapper.builder()
-        .addModule(kotlinModule())
-        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        .build()
+    val mapper: JsonMapper = JsonMapper.builder().addModule(kotlinModule())
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).build()
 
-    private fun getPreferences(context: Context): SharedPreferences =
-        context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+    private fun getPreferences(context: Context): SharedPreferences {
+        return context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+    }
 
-    fun Context.getSharedPrefs(): SharedPreferences = getPreferences(this)
-    fun Context.getDefaultSharedPrefs(): SharedPreferences =
-        PreferenceManager.getDefaultSharedPreferences(this)
+    fun Context.getSharedPrefs(): SharedPreferences {
+        return getPreferences(this)
+    }
 
-    fun getFolderName(folder: String, path: String): String = "$folder/$path"
+
+    fun getFolderName(folder: String, path: String): String {
+        return "${folder}/${path}"
+    }
 
     fun editor(context: Context, isEditingAppSettings: Boolean = false): Editor {
-        val editor = if (isEditingAppSettings) context.getDefaultSharedPrefs().edit()
-        else context.getSharedPrefs().edit()
+        val editor: SharedPreferences.Editor =
+            if (isEditingAppSettings) context.getDefaultSharedPrefs()
+                .edit() else context.getSharedPrefs().edit()
         return Editor(editor)
     }
 
-    fun Context.getKeys(folder: String): List<String> =
-        getSharedPrefs().all.keys.filter { it.startsWith(folder) }
+    fun Context.getDefaultSharedPrefs(): SharedPreferences {
+        return PreferenceManager.getDefaultSharedPreferences(this)
+    }
 
-    fun Context.removeKey(path: String) {
-        try { getSharedPrefs().edit { remove(path) } } catch (e: Exception) { logError(e) }
+    fun Context.getKeys(folder: String): List<String> {
+        return this.getSharedPrefs().all.keys.filter { it.startsWith(folder) }
     }
 
     fun Context.removeKey(folder: String, path: String) {
         removeKey(getFolderName(folder, path))
     }
 
+    fun Context.containsKey(folder: String, path: String): Boolean {
+        return containsKey(getFolderName(folder, path))
+    }
+
+    fun Context.containsKey(path: String): Boolean {
+        val prefs = getSharedPrefs()
+        return prefs.contains(path)
+    }
+
+    fun Context.removeKey(path: String) {
+        try {
+            val prefs = getSharedPrefs()
+            if (prefs.contains(path)) {
+                prefs.edit {
+                    remove(path)
+                }
+            }
+        } catch (e: Exception) {
+            logError(e)
+        }
+    }
+
     fun Context.removeKeys(folder: String): Int {
         val keys = getKeys("$folder/")
         try {
-            getSharedPrefs().edit { keys.forEach { remove(it) } }
+            getSharedPrefs().edit {
+                keys.forEach { value ->
+                    remove(value)
+                }
+            }
             return keys.size
         } catch (e: Exception) {
             logError(e)
@@ -109,49 +157,56 @@ object DataStore {
         }
     }
 
-    fun Context.containsKey(path: String): Boolean =
-        getSharedPrefs().contains(path)
-
-    fun Context.containsKey(folder: String, path: String): Boolean =
-        containsKey(getFolderName(folder, path))
-
-    /** =================
-     * Set Key Functions
-     * ================= */
     fun <T> Context.setKey(path: String, value: T) {
-        try { getSharedPrefs().edit { putString(path, mapper.writeValueAsString(value)) } }
-        catch (e: Exception) { logError(e) }
+        try {
+            getSharedPrefs().edit {
+                putString(path, mapper.writeValueAsString(value))
+            }
+        } catch (e: Exception) {
+            logError(e)
+        }
     }
 
-    fun <T> Context.setKey(folder: String, path: String, value: T) =
-        setKey(getFolderName(folder, path), value)
-
-    /** =================
-     * Get Key Functions
-     * ================= */
     fun <T> Context.getKey(path: String, valueType: Class<T>): T? {
-        return try {
-            val json = getSharedPrefs().getString(path, null) ?: return null
-            mapper.readValue(json, valueType)
-        } catch (e: Exception) { null }
+        try {
+            val json: String = getSharedPrefs().getString(path, null) ?: return null
+            return json.toKotlinObject(valueType)
+        } catch (e: Exception) {
+            return null
+        }
     }
 
-    inline fun <reified T : Any> String.toKotlinObject(): T =
-        mapper.readValue(this, T::class.java)
-
-    fun <T> String.toKotlinObject(valueType: Class<T>): T =
-        mapper.readValue(this, valueType)
-
-    inline fun <reified T : Any> Context.getKey(path: String, defVal: T? = null): T? {
-        return try {
-            val json = getSharedPrefs().getString(path, null) ?: return defVal
-            json.toKotlinObject<T>()
-        } catch (e: Exception) { defVal }
+    fun <T> Context.setKey(folder: String, path: String, value: T) {
+        setKey(getFolderName(folder, path), value)
     }
 
-    inline fun <reified T : Any> Context.getKey(folder: String, path: String, defVal: T? = null): T? =
-        getKey(getFolderName(folder, path), defVal)
+    inline fun <reified T : Any> String.toKotlinObject(): T {
+        return mapper.readValue(this, T::class.java)
+    }
 
-    inline fun <reified T : Any> Context.getKey(folder: String, path: String): T? =
-        getKey(getFolderName(folder, path), null)
+    fun <T> String.toKotlinObject(valueType: Class<T>): T {
+        return mapper.readValue(this, valueType)
+    }
+
+    // GET KEY GIVEN PATH AND DEFAULT VALUE, NULL IF ERROR
+    inline fun <reified T : Any> Context.getKey(path: String, defVal: T?): T? {
+        try {
+            val json: String = getSharedPrefs().getString(path, null) ?: return defVal
+            return json.toKotlinObject()
+        } catch (e: Exception) {
+            return null
+        }
+    }
+
+    inline fun <reified T : Any> Context.getKey(path: String): T? {
+        return getKey(path, null)
+    }
+
+    inline fun <reified T : Any> Context.getKey(folder: String, path: String): T? {
+        return getKey(getFolderName(folder, path), null)
+    }
+
+    inline fun <reified T : Any> Context.getKey(folder: String, path: String, defVal: T?): T? {
+        return getKey(getFolderName(folder, path), defVal) ?: defVal
+    }
 }
