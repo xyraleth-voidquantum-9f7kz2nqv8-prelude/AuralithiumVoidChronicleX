6c9a9eb717eae4699a9b90eb8df854183852ade3
xyraleth-voidquantum-9f7kz2nqv8-prelude <xyralethvoidquantum@gmail.com>
2026-01-29 06:33:17 +0700

    Update BackupUtils.kt

--- app/src/main/java/com/lagradost/cloudstream3/utils/BackupUtils.kt
+++ app/src/main/java/com/lagradost/cloudstream3/utils/BackupUtils.kt
@@ -1,73 +1,44 @@
 package com.lagradost.cloudstream3.utils
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.net.Uri
-import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.WorkerThread
-import androidx.core.net.toUri
 import androidx.fragment.app.FragmentActivity
-import androidx.preference.PreferenceManager
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.lagradost.cloudstream3.CloudStreamApp.Companion.getActivity
 import com.lagradost.cloudstream3.CommonActivity.showToast
-import com.lagradost.cloudstream3.R
 import com.lagradost.cloudstream3.mvvm.logError
 import com.lagradost.cloudstream3.plugins.PLUGINS_KEY
 import com.lagradost.cloudstream3.plugins.PLUGINS_KEY_LOCAL
 import com.lagradost.cloudstream3.syncproviders.AccountManager
 import com.lagradost.cloudstream3.syncproviders.providers.AniListApi.Companion.ANILIST_CACHED_LIST
 import com.lagradost.cloudstream3.syncproviders.providers.MALApi.Companion.MAL_CACHED_LIST
-import com.lagradost.cloudstream3.utils.Coroutines.ioSafe
-import com.lagradost.cloudstream3.utils.Coroutines.main
 import com.lagradost.cloudstream3.utils.DataStore.getDefaultSharedPrefs
 import com.lagradost.cloudstream3.utils.DataStore.getSharedPrefs
 import com.lagradost.cloudstream3.utils.DataStore.mapper
-import com.lagradost.cloudstream3.utils.UIHelper.checkWrite
-import com.lagradost.cloudstream3.utils.UIHelper.requestRW
-import com.lagradost.cloudstream3.utils.VideoDownloadManager.StreamData
-import com.lagradost.cloudstream3.utils.VideoDownloadManager.getBasePath
-import com.lagradost.cloudstream3.utils.VideoDownloadManager.setupStream
-import com.lagradost.safefile.MediaFileContentType
-import com.lagradost.safefile.SafeFile
-import okhttp3.internal.closeQuietly
-import java.io.IOException
-import java.io.OutputStream
-import java.io.PrintWriter
-import java.lang.System.currentTimeMillis
-import java.text.SimpleDateFormat
-import java.util.Date
+import com.lagradost.cloudstream3.utils.Coroutines.ioSafe
+import com.lagradost.cloudstream3.utils.Coroutines.main
 
 object BackupUtils {
 
     /**
-     * No sensitive or breaking data in the backup
-     * */
+     * Keys yang tidak boleh di-backup
+     */
     private val nonTransferableKeys = listOf(
         ANILIST_CACHED_LIST,
         MAL_CACHED_LIST,
-
-        // The plugins themselves are not backed up
         PLUGINS_KEY,
         PLUGINS_KEY_LOCAL,
-
         AccountManager.ACCOUNT_TOKEN,
         AccountManager.ACCOUNT_IDS,
-
-        "biometric_key", // can lock down users if backup is shared on a incompatible device
-        "nginx_user", // Nginx user key
-
-        // No access rights after restore data from backup
+        "biometric_key",
+        "nginx_user",
         "download_path_key",
         "download_path_key_visual",
         "backup_path_key",
         "backup_dir_path_key",
-
-        // When sharing backup we do not want to transfer what is essentially the password
-        // Note that this is deprecated, and can be removed after all tokens have expired
         "anilist_token",
         "anilist_user",
         "mal_user",
@@ -79,14 +50,13 @@ object BackupUtils {
         "simkl_token",
     )
 
-    /** false if key should not be contained in backup */
+    /** false jika key tidak boleh di-backup */
     private fun String.isTransferable(): Boolean {
         return !nonTransferableKeys.any { this.contains(it) }
     }
 
     private var restoreFileSelector: ActivityResultLauncher<Array<String>>? = null
 
-    // Kinda hack, but I couldn't think of a better way
     data class BackupVars(
         @JsonProperty("_Bool") val bool: Map<String, Boolean>?,
         @JsonProperty("_Int") val int: Map<String, Int>?,
@@ -101,6 +71,7 @@ object BackupUtils {
         @JsonProperty("settings") val settings: BackupVars
     )
 
+    /** Ambil data backup (tetap ada, tapi tidak dipakai) */
     @Suppress("UNCHECKED_CAST")
     private fun getBackup(context: Context?): BackupFile? {
         if (context == null) return null
@@ -132,7 +103,7 @@ object BackupUtils {
         )
     }
 
-    @WorkerThread
+    /** Restore tetap jalan seperti biasa */
     fun restore(
         context: Context?,
         backupFile: BackupFile,
@@ -158,66 +129,19 @@ object BackupUtils {
             context.restoreMap(backupFile.datastore.stringSet)
         }
 
-        // Make sure the library is fresh
+        // Refresh library syncApis
         for(api in AccountManager.syncApis) {
             api.requireLibraryRefresh = true
         }
     }
 
-    @SuppressLint("SimpleDateFormat")
+    /** ❌ Backup dinonaktifkan total */
     fun backup(context: Context?) = ioSafe {
-        if (context == null) return@ioSafe
-
-        var fileStream: OutputStream? = null
-        var printStream: PrintWriter? = null
-        try {
-            if (!context.checkWrite()) {
-                showToast(R.string.backup_failed, Toast.LENGTH_LONG)
-                context.getActivity()?.requestRW()
-                return@ioSafe
-            }
-
-            val date = SimpleDateFormat("yyyy_MM_dd_HH_mm").format(Date(currentTimeMillis()))
-            val displayName = "CS3_Backup_${date}"
-            val backupFile = getBackup(context)
-            val stream = setupBackupStream(context, displayName)
-
-            fileStream = stream.openNew()
-            printStream = PrintWriter(fileStream)
-            printStream.print(mapper.writeValueAsString(backupFile))
-
-            showToast(
-                R.string.backup_success,
-                Toast.LENGTH_LONG
-            )
-        } catch (e: Exception) {
-            logError(e)
-            try {
-                showToast(
-                    txt(R.string.backup_failed_error_format, e.toString()),
-                    Toast.LENGTH_LONG
-                )
-            } catch (e: Exception) {
-                logError(e)
-            }
-        } finally {
-            printStream?.closeQuietly()
-            fileStream?.closeQuietly()
-        }
+        // Tidak melakukan apa-apa
+        return@ioSafe
     }
 
-    @Throws(IOException::class)
-    private fun setupBackupStream(context: Context, name: String, ext: String = "txt"): StreamData {
-        return setupStream(
-            baseFile = getCurrentBackupDir(context).first ?: getDefaultBackupDir(context)
-            ?: throw IOException("Bad config"),
-            name,
-            folder = null,
-            extension = ext,
-            tryResume = false
-        )
-    }
-
+    /** Restore file selector tetap jalan */
     fun FragmentActivity.setUpBackup() {
         try {
             restoreFileSelector =
@@ -241,9 +165,10 @@ object BackupUtils {
                             activity.runOnUiThread { activity.recreate() }
                         } catch (e: Exception) {
                             logError(e)
-                            main { // smth can fail in .format
+                            main {
                                 showToast(
-                                    getString(R.string.restore_failed_format).format(e.toString())
+                                    getString(com.lagradost.cloudstream3.R.string.restore_failed_format)
+                                        .format(e.toString())
                                 )
                             }
                         }
@@ -286,29 +211,5 @@ object BackupUtils {
             }
         }
         editor.apply()
-    }
-
-    /**
-     * Copy of [VideoDownloadManager.basePathToFile], [VideoDownloadManager.getDefaultDir] and [VideoDownloadManager.getBasePath]
-     * modded for backup specific paths
-     * */
-
-    fun getDefaultBackupDir(context: Context): SafeFile? {
-        return SafeFile.fromMedia(context, MediaFileContentType.Downloads)
-    }
-
-    fun getCurrentBackupDir(context: Context): Pair<SafeFile?, String?> {
-        val settingsManager = PreferenceManager.getDefaultSharedPreferences(context)
-        val basePathSetting =
-            settingsManager.getString(context.getString(R.string.backup_path_key), null)
-        return baseBackupPathToFile(context, basePathSetting) to basePathSetting
-    }
-
-    private fun baseBackupPathToFile(context: Context, path: String?): SafeFile? {
-        return when {
-            path.isNullOrBlank() -> getDefaultBackupDir(context)
-            path.startsWith("content://") -> SafeFile.fromUri(context, path.toUri())
-            else -> SafeFile.fromFilePath(context, path)
-        }
     }
 }
